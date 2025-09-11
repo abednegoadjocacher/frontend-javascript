47872de771e35313a82bebd78eaa5c4c12ed0ed1
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-11 20:33:02 +0000

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,19 +1,25 @@
-interface Director {
-  workDirectorTasks(): string;
-}
-
-interface Teacher {
-  workTeacherTasks(): string;
-}
-
-function isDirector(employee: Teacher | Director): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-function executeWork(employee: Teacher | Director) {
-  if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
-  } else {
-    console.log(employee.workTeacherTasks());
+// Example Director and Teacher class definitions
+export class Director {
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
   }
+}
+
+export class Teacher {
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Type predicate
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director
+}
+
+// Executes tasks depending on role
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks()
+  }
+  return employee.workTeacherTasks()
 }
